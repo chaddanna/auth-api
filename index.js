0b57fd24c165bc7dd4859bc5aef10d2e6f8c8192
Chad Danna <chad@formulated.io>
2017-07-10 00:27:16 -0600

    Create base of json server

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import http from 'http'
+import bodyParser from 'body-parser'
+import morgan from 'morgan'
+const app = express()
+
+app.use(morgan('combined'))
+app.use(bodyParser.json({ type: '*/*' }))
+
+const port = process.env.PORT || 3090
+const server = http.createServer(app)
+server.listen(port)
+console.log('Server listening on', port)
