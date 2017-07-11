ca5450f3ff8f2a87c3164bf4fad2806c176ca3a4
Chad Danna <chad@formulated.io>
2017-07-10 21:41:31 -0600

    Connect to mongodb

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ import express from 'express'
 import http from 'http'
 import bodyParser from 'body-parser'
 import morgan from 'morgan'
-const app = express()
+import router from './router'
+import mongoose from 'mongoose'
 
+// Database
+mongoose.connect('mongodb://localhost/positive')
+
+// App
+const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json({ type: '*/*' }))
+router(app)
 
+// Server
 const port = process.env.PORT || 3090
 const server = http.createServer(app)
 server.listen(port)
