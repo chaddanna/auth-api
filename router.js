4c02f939231e2f119496caea62e5d13a34e3c365
Chad Danna <chad@formulated.io>
2017-07-17 14:27:59 -0600

    Create signin needs • Create passport authentication for signin • Create signin route

--- router.js
+++ router.js
@@ -3,11 +3,12 @@ import passportService from './services/passport'
 import passport from 'passport'
 
 const requireAuth = passport.authenticate('jwt', { session: false })
+const requireSignin = passport.authenticate('local', { session: false })
 
 export default (app) => {
   app.get('/', requireAuth, (req, res) => {
     res.send({ hi: 'there' })
   })
   app.post('/signup', Authentication.signup)
-  app.post('/signin', Authentication.signin)
+  app.post('/signin', requireSignin, Authentication.signin)
 }
